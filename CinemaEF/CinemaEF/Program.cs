b4831b01440f21cd4a5b22e781207e039256a067
Felicia <feliciasskolarbete@gmail.com>
2018-09-13 10:05:30 +0200

    +++ ??

--- CinemaEF/CinemaEF/Program.cs
+++ CinemaEF/CinemaEF/Program.cs
@@ -4,6 +4,9 @@ namespace CinemaEF
 {
     class Program
     {
+        private static CinemaContext context = new CinemaContext();
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
