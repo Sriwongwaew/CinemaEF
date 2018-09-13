c19b08e61772d6744ef77ef23a99d093f463c42d
LAPTOP-96UQLCFD\sriwo <sriwongwaew@gmail.com>
2018-09-13 09:15:52 +0200

    sdfsf

---
+++ CinemaEF/CinemaEF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CinemaEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
