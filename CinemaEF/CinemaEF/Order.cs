1e25bc0b5ae4079fed22fcb42d8ae05330d4f84b
LAPTOP-96UQLCFD\sriwo <sriwongwaew@gmail.com>
2018-09-13 10:03:52 +0200

    Added using and changed namespace

--- CinemaEF/CinemaEF/Order.cs
+++ CinemaEF/CinemaEF/Order.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CinemaEf;
 
-namespace CinemaEF
+namespace CinemaEf
 {
     public class Order
     {
