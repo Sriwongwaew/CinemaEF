99582a4a596d9345710d4bd49476f280ddeb0d0a
AWA5CG8126DFW\Administrator <isabelle.s.eriksson@gmail.com>
2018-09-13 13:45:38 +0200

    Program is back

---
+++ CinemaEF/CinemaEF/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CinemaEf;
+
+namespace CinemaEf
+{
+    class Program
+    {
+        private static CinemaContext context = new CinemaContext();
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
