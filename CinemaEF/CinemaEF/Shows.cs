3ced3f21a012d579254fc4e1470c8722e60b845f
LAPTOP-96UQLCFD\sriwo <sriwongwaew@gmail.com>
2018-09-13 09:45:29 +0200

    Shows

--- CinemaEF/CinemaEF/Shows.cs
+++ CinemaEF/CinemaEF/Shows.cs
@@ -8,6 +8,9 @@ namespace CinemaEF
 {
     public class Shows
     {
-        
+        public int Id { get; set; }
+        public Salon SalonId { get; set; }
+        public Movie MovieId { get; set; }
+        public DateTime StartTime { get; set; }
     }
 }
